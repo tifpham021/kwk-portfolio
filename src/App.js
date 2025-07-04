72042c16f8a380c85b3e6b99da76e30f0b11a3ec
tifpham021 <tpham346@gatech.edu>
2025-07-04 19:12:16 -0400

    did the intro part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import tiff from "./assets/tiff.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="landing-page">
+      <div className="intro">
+        <div className="pictures-left">
+          <div className="light-green"></div>
+          <img src={tiff} alt="profile-pic" className="pfp"/>
+          <div className="dark-green"></div>
+        </div>
+        <div className="text-right">
+          <h2>HEY, I'M</h2>
+          <h1 className="first-name">TIFFANY</h1>
+          <h1 className="last-name">PHAM</h1>
+        </div>
+      </div>
+      <div className="about">
+        <h2>ABOUT ME</h2>
+        <h1>ABOUT ME</h1>
+        <h2>ABOUT ME</h2>
+      </div>
     </div>
   );
 }
